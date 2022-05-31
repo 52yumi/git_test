fad5f2bd4c51a0e4a15bb6a3085840aeaaba23b7
stone <stone@qq.com>
2022-06-01 00:22:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author MagicBullet
+ * @version 1.0
+ * @date 2022/6/1 0:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello，Git");
+    }
+}
